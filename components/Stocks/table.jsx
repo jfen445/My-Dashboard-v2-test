d01c160b50941b78843a43edab6ec3bb84328c39
jfen445 <jfen445@aucklanduni.ac.nz>
2022-02-05 16:47:59 +1300

    file rename

--- components/Stocks/table.jsx
+++ components/Stocks/table.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 // import { useGet } from '../../utils/crudHooks';
-import SlideOver from "./slideOver";
+import SlideOver from "./slideOvers";
 
 const finnhub = require("finnhub");
 const api_key = finnhub.ApiClient.instance.authentications["api_key"];
